a7c158d5790a3a1f7142ae27cb10483a66cac596
Andy <92107784+MayMay1111@users.noreply.github.com>
2023-04-14 18:34:41 +0700

    Create main2.js

---
+++ custom/Ombre/js/main2.js
@@ -0,0 +1,26 @@
+jQuery(window).load(function () {
+            "use strict";
+            jQuery("#testimonials").show();
+            var owl = jQuery("#testimonials").owlCarousel({
+                items: 1,
+                autoplay: true,
+                autoplayTimeout: 4000,
+                autoplayHoverPause: true,
+                dots: false,
+                autoHeight: false,
+                margin: 100,
+                mouseDrag: false,
+                touchDrag: false,
+                smartSpeed: 800,
+                nav: false,
+                loop: true,
+                animateIn: 'fadeIn',
+                animateOut: 'fadeOut'
+            });
+            jQuery('#testimonials').find('.testimonial-nav-left .fa').click(function() {
+                owl.trigger('prev.owl.carousel');
+            });
+            jQuery('#testimonials').find('.testimonial-nav-right .fa').click(function() {
+                owl.trigger('next.owl.carousel');
+            });
+        });
