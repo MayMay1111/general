62534793ae052bf1f23e9ded0e27f180d8bc38f3
Andy <92107784+MayMay1111@users.noreply.github.com>
2023-04-14 18:36:31 +0700

    Create main4.js

---
+++ custom/Ombre/js/main4.js
@@ -0,0 +1,4 @@
+jQuery(window).load(function () {
+            "use strict";
+            jQuery('.grid.hero').backstretch("https://thememasters.club/html-templates/ombre/images/photos/testimonials.jpg");
+        });
