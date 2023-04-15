97dd6b8925ad4e800329ccc0df1503dc8eb8523f
Andy <92107784+MayMay1111@users.noreply.github.com>
2023-04-15 10:59:09 +0700

    Create featureBG.js

---
+++ custom/Ombre/js/featureBG.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function () {
+            "use strict";
+            jQuery('#featured-image').backstretch("https://thememasters.club/html-templates/ombre/images/photos/features.jpg", {fade:500});
+            jQuery('.grid.hero').backstretch("https://thememasters.club/html-templates/ombre/images/photos/crowd.jpg");
+        });
