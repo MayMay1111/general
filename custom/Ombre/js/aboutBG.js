229e239aa596fd9f6e2bc45e25d2f4881f68838c
Andy <92107784+MayMay1111@users.noreply.github.com>
2023-04-15 10:55:00 +0700

    Update aboutBG.js

--- custom/Ombre/js/aboutBG.js
+++ custom/Ombre/js/aboutBG.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function () {
             "use strict";
-            jQuery('#featured-image').backstretch("images/photos/about.jpg", {fade:500});
+            jQuery('#featured-image').backstretch("https://thememasters.club/html-templates/ombre/images/photos/about.jpg", {fade:500});
             jQuery('.grid.hero').backstretch("images/photos/crowd.jpg");
         });
