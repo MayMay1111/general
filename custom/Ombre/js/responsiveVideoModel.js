d1ca4ede01c273b1a90a19c8dc9c8a04d833ce0c
Andy <92107784+MayMay1111@users.noreply.github.com>
2023-04-15 11:10:42 +0700

    Create responsiveVideoModel.js

---
+++ custom/Ombre/js/responsiveVideoModel.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function () {
+            jQuery(".flex-video").prettyEmbed({
+                customPreviewImage: 'images/photos/img4.jpg',
+                // Embed controls
+                showInfo: false,
+                showControls: true,
+                loop: false,
+                showRelated: false,
+                useFitVids: true
+            });
+        });
