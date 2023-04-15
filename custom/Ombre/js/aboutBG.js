de156fb13acc34c634dd6bbc888b88f879f9cea5
Andy <92107784+MayMay1111@users.noreply.github.com>
2023-04-15 10:17:41 +0700

    Create aboutBG.js

---
+++ custom/Ombre/js/aboutBG.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function () {
+            "use strict";
+            jQuery('#featured-image').backstretch("images/photos/about.jpg", {fade:500});
+            jQuery('.grid.hero').backstretch("images/photos/crowd.jpg");
+        });
