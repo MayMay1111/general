5d68a4674ff1c9ce29ff11407e9663366eeca7c9
Andy <92107784+MayMay1111@users.noreply.github.com>
2023-04-17 18:26:54 +0700

    Create fixOurPartners.js

---
+++ custom/Ombre/js/fixOurPartners.js
@@ -0,0 +1,26 @@
+setTimeout(function(){
+jQuery("#partners").owlCarousel({
+                autoplay: true,
+                autoplayTimeout: 4000,
+                autoplayHoverPause: true,
+                items: 1,
+                margin: 20,
+                dots: false,
+                smartSpeed: 800,
+                navText: ['', ''],
+                nav: false,
+                loop: true,
+                lazyLoad: true,
+                responsive: {
+                    341: {
+                        items: 2
+                    },
+                    641: {
+                        items: 3
+                    },
+                    1025: {
+                        items: 4
+                    }
+                }
+            });
+},1000)
